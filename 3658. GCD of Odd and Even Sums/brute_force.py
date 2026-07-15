9d14f857cff2cdb330e548dae2b584a43d54a5de
Om Apar <omapar0123@gmail.com>
2026-07-15 18:47:18 +0530

    Refactor gcdOfOddEvenSums function to improve clarity and structure

--- 3658. GCD of Odd and Even Sums/brute_force.py
+++ 3658. GCD of Odd and Even Sums/brute_force.py
@@ -1,3 +1,20 @@
 class Solution:
     def gcdOfOddEvenSums(self, n: int) -> int:
         
+        odd_sum = 0
+        even_sum = 0
+
+        for i in range(1,(n + n + 1)):
+            if i % 2 == 0:
+                even_sum += i
+            else:
+                odd_sum += i
+        
+        gcd = 1
+
+        for i in range(1, min(odd_sum, even_sum) + 1):
+            if odd_sum % i == 0 and even_sum % i == 0:
+                gcd = i
+
+        return gcd
+        
