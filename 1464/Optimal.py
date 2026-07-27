7dc60436b5ac42ad2c48310e160ef06462b6112b
Om Apar <omapar0123@gmail.com>
2026-07-27 23:41:32 +0530

    Add detailed explanation of maxProduct solution approach

--- 1464/Optimal.py
+++ 1464/Optimal.py
@@ -11,3 +11,11 @@ class Solution:
                 second = num
 
         return (first - 1) * (second - 1)
+
+
+'''
+How I solved it:
+    1. I initially thought of sorting the array and then taking the last two elements to calculate the product. However, that would take O(n log n) time complexity.
+    2. Instead, I realized that I can find the two largest numbers in a single pass through the array, which would take O(n) time complexity.   
+
+'''
